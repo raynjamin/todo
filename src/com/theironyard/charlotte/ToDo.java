a615375c784e96dbc80bd562c72284423e333fd3
Ben Sterrett <raynjamin@gmail.com>
2017-01-10 10:55:39 -0500

    Adding select by id method to todos

--- src/com/theironyard/charlotte/ToDo.java
+++ src/com/theironyard/charlotte/ToDo.java
@@ -1,12 +1,61 @@
 package com.theironyard.charlotte;
 
+import org.h2.tools.Server;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDo {
+    public static void insertToDo(Connection conn, String text) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES (NULL, ?, false)");
+        stmt.setString(1, text);
+        stmt.execute();
+    }
 
-    public static void main(String[] args) {
+    public static ArrayList<ToDoItem> selectToDos(Connection conn) throws SQLException {
         ArrayList<ToDoItem> items = new ArrayList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM todos");
+        while (results.next()) {
+            int id = results.getInt("id");
+            String text = results.getString("text");
+            boolean isDone = results.getBoolean("is_done");
+            items.add(new ToDoItem(id, text, isDone));
+        }
+        return items;
+    }
+
+    public static ToDoItem getToDoById(Connection conn, int id) throws SQLException {
+        ToDoItem item = null;
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM todos where id=?");
+        stmt.setInt(1, id);
+
+        ResultSet results = stmt.executeQuery();
+
+        if (results.next()) {
+            String text = results.getString("text");
+            boolean isDone = results.getBoolean("is_done");
+            item = new ToDoItem(id, text, isDone);
+        }
+
+        return item;
+    }
+
+
+    public static void toggleToDo(Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE todos SET is_done = NOT is_done WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, text VARCHAR, is_done BOOLEAN)");
+
+//        ArrayList<ToDoItem> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -20,23 +69,27 @@ public class ToDo {
                 System.out.println("Enter your to-do item:");
                 String text = scanner.nextLine();
 
-                ToDoItem item = new ToDoItem(text, false);
-                items.add(item);
+//                ToDoItem item = new ToDoItem(text, false);
+//                items.add(item);
+
+                insertToDo(conn, text);
             }
 
             else if (option.equals("2")) {
                 System.out.println("Enter the number of the item you want to toggle:");
                 int itemNum = Integer.valueOf(scanner.nextLine());
-                ToDoItem item = items.get(itemNum - 1);
-                item.isDone = !item.isDone;
+//                ToDoItem item = items.get(itemNum - 1);
+//                item.isDone = !item.isDone;
+
+                toggleToDo(conn, itemNum);
             }
 
             else if (option.equals("3")) {
-                for (int i = 1;i <= items.size();i++) {
-                    ToDoItem item = items.get(i - 1);
+                ArrayList<ToDoItem> items = selectToDos(conn);
 
+                for (ToDoItem item : items) {
                     // <boolean value> ? <thing to return if boolean is true> : <thing to return if boolean is false>
-                    System.out.printf("[%s] %d. %s\n", item.isDone ? "x" : " ", i, item.text);
+                    System.out.printf("[%s] %d. %s\n", item.isDone ? "x" : " ", item.id, item.text);
                 }
             }
 
--- src/com/theironyard/charlotte/ToDoItem.java
+++ src/com/theironyard/charlotte/ToDoItem.java
@@ -3,8 +3,19 @@ package com.theironyard.charlotte;
 public class ToDoItem {
     public String text;
     public boolean isDone;
+    public int id;
+
+
+    public ToDoItem() {
+    }
 
     public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
